40dcf91f398f844784660d2cff1363857ea04249
Juan Falquez <jfalquez@gwmail.gwu.edu>
2012-02-18 03:21:30 +0000

    RPC done.. and am rather happy with it.
    git-svn-id: svn+ssh://robotics.gwu.edu/home/rpg/repos/RPG/dev@135 e56edf37-dbb1-46d4-901c-b18800ef280d

--- Nodes/Node.h
+++ Nodes/Node.h
@@ -4,7 +4,8 @@
 #include <map>
 #include <string>
 #include <sstream>
-#include <iostream> // not needed
+#include <iostream>
+#include <boost/thread.hpp>
 
 #include <zmq.hpp>
 
@@ -12,18 +13,28 @@
 
 #define MASTER 127.0.0.1:1337
 
-typedef void(*FuncPtr)(std::string&,std::string&);
-
 namespace rpg
 {
 
     class Node
     {
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        typedef void(*FuncPtr)( google::protobuf::Message&, google::protobuf::Message& );
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        struct RPC {
+            FuncPtr                     RpcFunc;
+            google::protobuf::Message*  ReqMsg;
+            google::protobuf::Message*  RepMsg;
+        };
+
 
     public:
 
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         Node(
-            int nPort       //< Input: RPC Port
+            int nPort       //< Input: RPC Port.. randomized in the future
             )
         {
             // init context
@@ -31,224 +42,111 @@ namespace rpg
             m_pRpcSocket = new zmq::socket_t( *m_pContext, ZMQ_REP );
             std::ostringstream address;
             address << "tcp://*:" << nPort;
-            std::cout << "RPC Listener at " << address.str() << std::endl;
+            std::cout << "[Node] RPC listener at " << address.str() << std::endl;
             m_pRpcSocket->bind( address.str().c_str() );
             m_mHosts[ address.str() ] = m_pRpcSocket;
             // run listener in a different thread...
-            // how to handle data synch tho????????
+            boost::thread RPCThread( RPCThreadFunc, this );
         }
 
 
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         ~Node()
         {
         }
 
 
-        bool Publish(
-                std::string sTopic,             //< Input:
-                unsigned int nPort              //< Input: for now port, later it is random
-                )
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        static void RPCThreadFunc( Node *pThis )
         {
-            std::map < std::string, zmq::socket_t* >::iterator Sock;
+            while( 1 ) {
+                // wait for request
+                zmq::message_t ZmqReq;
+                if( !pThis->m_pRpcSocket->recv( &ZmqReq ) ) {
+                    // error receiving
+                    exit(1);
+                }
+                // obtain "header" which contains function name
+                unsigned char FuncNameSize;
+                memcpy( &FuncNameSize, ZmqReq.data(), sizeof(FuncNameSize) );
+                std::string FuncName( (char*)(ZmqReq.data()) + sizeof(FuncNameSize), FuncNameSize );
 
-            // check if socket is already open for this topic
-            Sock = m_mTopics.find( sTopic );
-            if( Sock != m_mTopics.end() ) {
-                // socket is already open, return false
+                // prepare reply message
+                unsigned int PbOffset = sizeof(FuncNameSize) + FuncNameSize;
+                unsigned int PbByteSize = ZmqReq.size() - PbOffset;
+
+                // look-up function
+                std::map < std::string, RPC* >::iterator it;
+
+                it = pThis->m_mRpcTable.find( FuncName );
+                if( it != pThis->m_mRpcTable.end() ) {
+
+                    RPC* pRPC = it->second;
+                    FuncPtr Func = pRPC->RpcFunc;
+                    google::protobuf::Message* Req = pRPC->ReqMsg;
+                    google::protobuf::Message* Rep = pRPC->RepMsg;
+
+                    if( !Req->ParseFromArray( (char*)ZmqReq.data() + PbOffset, PbByteSize ) ) {
+                        continue;
+                    }
+
+                    // call function
+                    (*Func)( *(Req),*(Rep) );
+
+                    // send reply
+                    zmq::message_t ZmqRep( Rep->ByteSize() );
+                    if( !Rep->SerializeToArray( ZmqRep.data(), Rep->ByteSize() ) ) {
+                        // error serializing protobuf to ZMQ message
+                    }
+                    pThis->m_pRpcSocket->send( ZmqRep );
+                } else {
+                    // send empty reply
+                    zmq::message_t ZmqRep(0);
+                    pThis->m_pRpcSocket->send( ZmqRep );
+                }
+            }
+        }
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        template <class Req, class Rep>
+        bool Register(
+		const std::string& sName,		//< Input: Function name
+		void(*pFunc)(Req&,Rep&)			//< Input: Function pointer
+		)
+        {
+            // check if function with that name is already registered
+            std::map < std::string, RPC* >::iterator it;
+            it = m_mRpcTable.find( sName );
+            if( it != m_mRpcTable.end() ) {
                 return false;
             } else {
-                // no socket open.. lets open a new one
-
-                // check if port is already in use
-                std::ostringstream address;
-                address << "tcp://*:" << nPort;
-                Sock = m_mHosts.find( address.str() );
-                if( Sock != m_mHosts.end() ) {
-                    // port is in use, return false
-                    return false;
-                }
-
-                // create socket
-                zmq::socket_t* pSock = new zmq::socket_t( *m_pContext, ZMQ_PUB );
-                try {
-                    std::cout << "Publishing topic " << sTopic << " on " << address.str() << std::endl;
-                    pSock->bind(address.str().c_str());
-                }
-                catch( zmq::error_t error ) {
-                    // oops, an error occurred lets rollback
-                    delete pSock;
-                    return false;
-                }
-                m_mHosts[ address.str() ] = pSock;
-                m_mTopics[ sTopic ] = pSock;
+                RPC* pRPC = new RPC;
+                pRPC->RpcFunc = (FuncPtr)pFunc;
+		pRPC->ReqMsg = new Req;
+		pRPC->RepMsg = new Rep;
+                m_mRpcTable[ sName ] = pRPC;
                 return true;
             }
         }
 
 
-        bool Write(
-                std::string sTopic,             //< Input:
-                google::protobuf::Message& Msg  //< Input:
-                )
-        {
-            std::map < std::string, zmq::socket_t* >::iterator Sock;
-
-            // check if socket is already open for this topic
-            Sock = m_mTopics.find( sTopic );
-            if( Sock == m_mTopics.end() ) {
-                // no socket found
-                return false;
-            } else {
-                zmq::socket_t* pSock = (*Sock).second;
-                zmq::message_t ZmqMsg( Msg.ByteSize() );
-                if( !Msg.SerializeToArray( ZmqMsg.data(), Msg.ByteSize() ) ) {
-                    // error serializing protobuf to ZMQ message
-                    return false;
-                }
-                return pSock->send( ZmqMsg );
-            }
-        }
-
-
-        bool Subscribe(
-                const std::string& sTopic,      //< Input:
-                const std::string& sHost        //< Input: for now, host info.. later, just the topic
-                )
-        {
-            std::map < std::string, zmq::socket_t* >::iterator Sock;
-
-            // check if socket is already open for this topic
-            Sock = m_mTopics.find( sTopic );
-            if( Sock != m_mTopics.end() ) {
-                // subscription for that topic already exists
-                return false;
-            } else {
-                // check if host+port is already in use
-                Sock = m_mHosts.find( sHost );
-                if( Sock != m_mHosts.end() ) {
-                    // port is in use, return false
-                    return false;
-                }
-                // create socket
-                zmq::socket_t* pSock = new zmq::socket_t( *m_pContext, ZMQ_SUB );
-
-                // lets connect using the socket
-                try {
-                    pSock->setsockopt( ZMQ_SUBSCRIBE, NULL, 0 );
-                    pSock->connect( ("tcp://" + sHost).c_str() );
-                }
-                catch( zmq::error_t error ) {
-                    // oops, an error occurred lets rollback
-                    delete pSock;
-                    return false;
-                }
-                m_mHosts[ sHost ] = pSock;
-                m_mTopics[ sTopic ] = pSock;
-                return true;
-            }
-        }
-
-
-        bool Read(
-                std::string sTopic,             //< Input:
-                google::protobuf::Message& Msg  //< Input:
-                )
-        {
-            std::map < std::string, zmq::socket_t* >::iterator Sock;
-
-            // check if socket is already open for this topic
-            Sock = m_mTopics.find( sTopic );
-            if( Sock == m_mTopics.end() ) {
-                // no socket found
-                return false;
-            } else {
-                zmq::socket_t* pSock = (*Sock).second;
-                zmq::message_t ZmqMsg;
-                if( !pSock->recv( &ZmqMsg, ZMQ_NOBLOCK ) ) {
-                    return false;
-                }
-                if( !Msg.ParseFromArray( ZmqMsg.data(), ZmqMsg.size() ) ) {
-                    return false;
-                }
-                return true;
-            }
-        }
-
-
-        bool RegisterService(
-                const std::string& sFuncName,                   //< Input: name of function we will call
-                void (*pFunc)(std::string&,std::string&)        //< Input: Pointer to function we will call
-                )
-        {
-            std::map < std::string, FuncPtr >::iterator Func;
-
-            // check if func is already registered
-            Func = m_mFuncTable.find( sFuncName );
-            if( Func != m_mFuncTable.end() ) {
-                return false;
-            } else {
-                m_mFuncTable[ sFuncName ] = pFunc;
-                return true;
-            }
-        }
-
-
-        bool ProvideService( )
-        {
-            // wait for request
-            zmq::message_t ZmqReq;
-            if( !m_pRpcSocket->recv( &ZmqReq ) ) {
-                // error receiving
-                return false;
-            }
-            // obtain "header" which contains function name
-            unsigned char FuncNameSize;
-            memcpy( &FuncNameSize, ZmqReq.data(), sizeof(FuncNameSize) );
-            std::string FuncName( (char*)(ZmqReq.data()) + sizeof(FuncNameSize), FuncNameSize );
-
-            // prepare reply message
-            unsigned int PbOffset = sizeof(FuncNameSize) + FuncNameSize;
-            unsigned int PbByteSize = ZmqReq.size() - PbOffset;
-
-            // prepare parameters for function call
-            std::string MsgReq( (char*)(ZmqReq.data()) + PbOffset, PbByteSize );
-            std::string MsgRep;
-
-            // look-up function
-            std::map < std::string, FuncPtr >::iterator Func;
-
-            Func = m_mFuncTable.find( FuncName );
-            if( Func != m_mFuncTable.end() ) {
-                // function exists... call it
-                (*(*Func).second)( MsgReq, MsgRep );
-            }
-
-            // prepare return message
-            zmq::message_t ZmqRep( MsgRep.size() );
-
-            memcpy( ZmqRep.data(), MsgRep.c_str(), MsgRep.size() );
-
-            // send reply
-            return m_pRpcSocket->send( ZmqRep );
-        }
-
-
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         bool Call(
-                const std::string& sHost,                   //< Input:
-                const std::string& sFuncName,               //< Input:
-                const google::protobuf::Message& MsgReq,    //< Input:
-                google::protobuf::Message& MsgRep           //< Output:
+                const std::string& sHost,                   //< Input: Remote host address <IP>:<PORT>
+                const std::string& sFuncName,               //< Input: Remote function to call
+                const google::protobuf::Message& MsgReq,    //< Input: Protobuf message containing request
+                google::protobuf::Message& MsgRep           //< Output: Protobuf message holding reply
                 )
         {
-            std::map < std::string, zmq::socket_t* >::iterator Sock;
+            std::map < std::string, zmq::socket_t* >::iterator it;
             zmq::socket_t* pSock;
 
-
             // check if socket is already open for this host
-            Sock = m_mHosts.find( sHost );
-            if( Sock != m_mHosts.end() ) {
+            it = m_mHosts.find( sHost );
+            if( it != m_mHosts.end() ) {
                 // socket is already open, lets use it
-                pSock = (*Sock).second;
+                pSock = it->second;
             } else {
                 // socket is not open, lets open one
                 pSock = new zmq::socket_t( *m_pContext, ZMQ_REQ );
@@ -300,6 +198,141 @@ namespace rpg
         }
 
 
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        bool Publish(
+                const std::string& sTopic,      	//< Input: Topic name
+                unsigned int nPort              	//< Input: for now port to publish, later it is random
+                )
+        {
+            std::map < std::string, zmq::socket_t* >::iterator it;
+
+            // check if socket is already open for this topic
+            it = m_mTopics.find( sTopic );
+            if( it != m_mTopics.end() ) {
+                // socket is already open, return false
+                return false;
+            } else {
+                // no socket open.. lets open a new one
+
+                // check if port is already in use
+                std::ostringstream address;
+                address << "tcp://*:" << nPort;
+                it = m_mHosts.find( address.str() );
+                if( it != m_mHosts.end() ) {
+                    // port is in use, return false
+                    return false;
+                }
+
+                // create socket
+                zmq::socket_t* pSock = new zmq::socket_t( *m_pContext, ZMQ_PUB );
+                try {
+                    std::cout << "[Node] Publishing topic '" << sTopic << "' on " << address.str() << std::endl;
+                    pSock->bind(address.str().c_str());
+                }
+                catch( zmq::error_t error ) {
+                    // oops, an error occurred lets rollback
+                    delete pSock;
+                    return false;
+                }
+                m_mHosts[ address.str() ] = pSock;
+                m_mTopics[ sTopic ] = pSock;
+                return true;
+            }
+        }
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        bool Write(
+                const std::string& sTopic,          	//< Input: Topic to write to
+                const google::protobuf::Message& Msg  	//< Input: Message to send
+                )
+        {
+            std::map < std::string, zmq::socket_t* >::iterator it;
+
+            // check if socket is already open for this topic
+            it = m_mTopics.find( sTopic );
+            if( it == m_mTopics.end() ) {
+                // no socket found
+                return false;
+            } else {
+                zmq::socket_t* pSock = it->second;
+                zmq::message_t ZmqMsg( Msg.ByteSize() );
+                if( !Msg.SerializeToArray( ZmqMsg.data(), Msg.ByteSize() ) ) {
+                    // error serializing protobuf to ZMQ message
+                    return false;
+                }
+                return pSock->send( ZmqMsg );
+            }
+        }
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        bool Subscribe(
+                const std::string& sTopic,      //< Input: Topic to subscribe to
+                const std::string& sHost        //< Input: for now, host info.. later, just the topic
+                )
+        {
+            std::map < std::string, zmq::socket_t* >::iterator it;
+
+            // check if socket is already open for this topic
+            it = m_mTopics.find( sTopic );
+            if( it != m_mTopics.end() ) {
+                // subscription for that topic already exists
+                return false;
+            } else {
+                // check if host+port is already in use
+                it = m_mHosts.find( sHost );
+                if( it != m_mHosts.end() ) {
+                    // port is in use, return false
+                    return false;
+                }
+                // create socket
+                zmq::socket_t* pSock = new zmq::socket_t( *m_pContext, ZMQ_SUB );
+
+                // lets connect using the socket
+                try {
+                    pSock->setsockopt( ZMQ_SUBSCRIBE, NULL, 0 );
+                    pSock->connect( ("tcp://" + sHost).c_str() );
+                }
+                catch( zmq::error_t error ) {
+                    // oops, an error occurred lets rollback
+                    delete pSock;
+                    return false;
+                }
+                m_mHosts[ sHost ] = pSock;
+                m_mTopics[ sTopic ] = pSock;
+                return true;
+            }
+        }
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        bool Read(
+                const std::string& sTopic,             	//< Input: Topic to read
+                google::protobuf::Message& Msg  	//< Output: Message read
+                )
+        {
+            std::map < std::string, zmq::socket_t* >::iterator it;
+
+            // check if socket is already open for this topic
+            it = m_mTopics.find( sTopic );
+            if( it == m_mTopics.end() ) {
+                // no socket found
+                return false;
+            } else {
+                zmq::socket_t* pSock = it->second;
+                zmq::message_t ZmqMsg;
+                if( !pSock->recv( &ZmqMsg, ZMQ_NOBLOCK ) ) {
+                    return false;
+                }
+                if( !Msg.ParseFromArray( ZmqMsg.data(), ZmqMsg.size() ) ) {
+                    return false;
+                }
+                return true;
+            }
+        }
+
+
     private:
 
         zmq::context_t*                         m_pContext;     // global context
@@ -309,9 +342,9 @@ namespace rpg
         // Variables for Topics (PubSubs)
         std::map< std::string, zmq::socket_t* > m_mTopics;      // map of topics + endpoints
 
-        // Variables for Services
-        zmq::socket_t*                          m_pRpcSocket;   // global RPC socket (port 1337)
-        std::map< std::string, FuncPtr >        m_mFuncTable;   // map of topics + functions
+        // Variables for RPC (ReqRep)
+        zmq::socket_t*                          m_pRpcSocket;   // global RPC socket
+        std::map< std::string, RPC* >           m_mRpcTable;    // map of functions + RPC structs
 
     };
 
--- Nodes/node1.cpp
+++ Nodes/node1.cpp
@@ -4,18 +4,11 @@
 
 #include <stdio.h>
 
-
-void TestFunc(std::string& X, std::string& Y) {
-    // this is needed at the beginning of each func
-    Professor MsgX, MsgY;
-    MsgX.ParseFromString(X);
-
-    printf("Professor %s owns!\n", MsgX.name().c_str());
-    MsgY.set_name("Pitufo");
-    MsgY.set_id(2);
-
-    // this is needed at the end of each func
-    MsgY.SerializeToString(&Y);
+void TestFunc( Professor& X, Student& Y )
+{
+    printf("Professor %s owns!\n", X.name().c_str());
+    Y.set_name("Pitufo");
+    Y.set_id(2);
 }
 
 
@@ -32,9 +25,7 @@ int main() {
     n = Test.Publish("LeftLeg", 5002);
     printf("1st publisher: %d\n",n);
 
-    n = Test.RegisterService( "Owns", &TestFunc );
-
-    n = Test.ProvideService();
+    n = Test.Register( "Owns", &TestFunc );
 
     while(1) {
         n = Test.Write("LeftLeg", SysAdmin);
@@ -43,3 +34,162 @@ int main() {
     }
     return 0;
 }
+
+#if 0
+#include "Testmsgs.pb.h"
+#include <iostream>
+#include <zmq.hpp>
+
+#include <google/protobuf/message.h>
+using namespace std;
+using namespace google::protobuf;
+
+/*
+template <class ReqType, class RepType>
+struct FunctionHandler
+{
+    RepType* (*m_pFunc)( ReqType* );
+//    static zmq::message_t& (*m_pEncodeFunc)( const Rep_type& );
+//    static Req_type (*m_pDecodeFunc)( const zmq::message_t& );
+};
+
+template <class T >
+static T DecodeFunc( const zmq::message_t& ZmqMsg )
+{
+    T t;
+    t.ParseFromArray( ZmqMsg.data(), ZmqMsg.size() );
+    return t;
+}
+
+template <class T >
+static zmq::message_t DecodeFunc( const T& t )
+{
+    zmq::message_t msg( t.ByteSize() );
+    t.SerialToArray( msg.data(), msg.size() );
+    return msg;
+}
+*/
+
+
+//typedef Message(*FuncPtr)(Message&);
+typedef void(*FuncPtr)(Message&,Message&);
+typedef Message*(*ParsePtr)(zmq::message_t* );
+
+map < string, FuncPtr >      mFuncTable;
+map < string, ParsePtr >     mParseTable;
+map < string, Message* >    mReqPbTable;
+map < string, Message* >    mRepPbTable;
+
+
+
+template <class T>
+struct ParseFuncWrapper
+{
+    static T* AllocAndParseFunc( zmq::message_t *msg )
+    {
+        T* pb = new T;
+        if( msg ){
+            pb->ParseFromArray( msg->data(), msg->size() );
+        }
+        return pb;
+    }
+};
+
+
+template <class Req, class Rep>
+struct AllocPBs
+{
+    static Req* AllocateReq()
+    {
+        Req* pb = new Req;
+        return pb;
+    }
+
+    static Rep* AllocateRep()
+    {
+        Rep* pb = new Rep;
+        return pb;
+    }
+};
+
+
+//Student TestFunc( Professor&  )
+//{
+//    Professor y;
+//    cout << y.GetTypeName();
+//    Student s;
+//    return s;
+//}
+
+void TestFunc( Professor& y, Student& s )
+{
+    cout << y.GetTypeName() << " and  " << s.GetTypeName() << endl;
+    s.set_id(3);
+}
+
+
+template <class Req, class Rep>
+//void Register( const std::string& sName, Rep(*pFunc)(Req&) )
+void Register( const std::string& sName, void(*pFunc)(Req&,Rep&) )
+{
+//    mParseTable[sName] = (ParsePtr)ParseFuncWrapper<Req>::AllocAndParseFunc;
+    mFuncTable[sName]  = (FuncPtr)pFunc;
+    mReqPbTable[sName] = AllocPBs<Req,Rep>::AllocateReq();
+    mRepPbTable[sName] = AllocPBs<Req,Rep>::AllocateRep();
+}
+
+
+
+
+int main()
+{
+
+
+//    A.m_pFunc = TestFunc;
+//    mFuncTable["Test"] = (FuncPtr)TestFunc;
+//    mParseTable["Test"] = (ParsePtr)MyParseFunc;
+
+    Register( "Test", TestFunc );
+
+    ParsePtr f = mParseTable["Test"];
+
+    zmq::message_t* msg = 0;
+//    Message* req = (*f)( msg );
+
+//    mReqPbTable["Test"]->ParseFromArray(msg->data(), msg->size());
+
+//    mReqPbTable["Test"] = new Professor();
+
+    FuncPtr fun = mFuncTable["Test"];
+
+//    Message* rep = new Message();
+
+//    Message* rep = (*fun)( *req );
+//    Message* rep = mEncodeTable["Test"];
+//    (*fun)( *req,*(mRepPbTable["Test"]) );
+    (*fun)( *(mReqPbTable["Test"]),*(mRepPbTable["Test"]) );
+
+//    delete req;
+
+//    map< string, Message* > m;
+//    m["123"] = new Professor;
+//    cout << m["123"]->GetTypeName() << endl;
+
+
+//    Message& pb = X;
+
+
+//    mFuncTables["Test"] = (FunctionHandler<Message,Message>*)&A;
+
+//    Professor X;
+//    (*mFuncTables["Test"]->m_pFunc)(X);
+
+//    map < string, Message* >      mFuncTables;
+
+//    mFuncTables["Test"] = &X;
+
+
+    return 0;
+
+}
+#endif
